d439ada58b0deac30ae24492f80b8863fbef7322
Tommy Hentschel <th584@cornell.edu>
2023-12-02 18:30:36 -0500

    Editing setup.py for correct description

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Analysis of inverting the dynamic structure factor to obtain a collision frequency, based on the Me.',
-    author='Thomas Hentschel',
-    license='MIT',
+    version="0.1.0",
+    description="""Analysis of inverting the dynamic structure factor to obtain
+        a collision frequency, based on the Meermin dielectric function.""",
+    author="Thomas Hentschel",
+    license="MIT",
 )
