41646ac70a801aea959765c9f49f0230ac2adef2
Tommy Hentschel <th584@cornell.edu>
2023-12-02 18:15:40 -0500

    Recreating setup.py for installing local packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of inverting the dynamic structure factor to obtain a collision frequency, based on the Me.',
+    author='Thomas Hentschel',
+    license='MIT',
+)
